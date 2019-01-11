045f1118162f6c18eeb44f767534226efaf55162
Davide Canton <davide.canton5@gmail.com>
2019-01-11 23:46:27 +0100

    added conditional operators

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import { Constructable, JsonMapper } from 'at-json';
-import { Observable, OperatorFunction } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { iif, Observable, of, OperatorFunction } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
+
 
 export function mapJsonModel<T, U>(ctor: Constructable<U>): OperatorFunction<T, U> {
     return (source: Observable<T>) => {
@@ -14,6 +15,34 @@ export function mapJsonArray<T, U>(ctor: Constructable<U>): OperatorFunction<T[]
     return (source: Observable<T[]>) => {
         return source.pipe(
             map(v => JsonMapper.deserializeArray(ctor, v || []))
+        );
+    };
+}
+
+export function conditionaMapJsonModel<T, T2, U>(predicate: (value: T) => boolean, ctor: Constructable<U>, selector: (value: T) => T2, defaultValue: U): OperatorFunction<T, U> {
+    return (source: Observable<T>) => {
+        return source.pipe(
+            mergeMap(value => {
+                return iif(
+                    () => predicate(value),
+                    of(selector(value)).pipe(mapJsonModel(ctor)),
+                    of(defaultValue)
+                );
+            })
+        );
+    };
+}
+
+export function conditionaMapJsonArray<T, T2, U>(predicate: (value: T[]) => boolean, ctor: Constructable<U>, selector: (value: T) => T2, defaultNull = false): OperatorFunction<T[] | null, U[]> {
+    return (source: Observable<T[]>) => {
+        return source.pipe(
+            mergeMap(value => {
+                return iif(
+                    () => predicate(value),
+                    of(value.map(selector)).pipe(mapJsonArray(ctor)),
+                    of(defaultNull ? null : [])
+                );
+            })
         );
     };
 }
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,8 @@
 import { JsonClass, JsonProperty, SerializeFn } from 'at-json';
 import { expect } from 'chai';
 import { of } from 'rxjs';
-import { mapJsonModel, mapJsonArray } from '../lib';
+
+import { conditionaMapJsonArray, conditionaMapJsonModel, mapJsonArray, mapJsonModel } from '../lib';
 
 @JsonClass(true)
 class Address {
@@ -51,6 +52,99 @@ describe('tests', () => {
                 expect(v.line2).to.equal(d[i].line2);
             });
 
+            done();
+        });
+    });
+
+    it('should deserialize if condition is met', done => {
+        const obj = {
+            C: true,
+            D: {
+                line1: 'a',
+                line2: 'b'
+            }
+        };
+
+        const NO_MAP = <Address>{};
+
+        of(obj).pipe(
+            conditionaMapJsonModel(v => v.C, Address, v => v.D, NO_MAP)
+        ).subscribe(v => {
+            expect(v).to.be.instanceOf(Address);
+            expect(v.line1).to.equal(obj.D.line1);
+            expect(v.line2).to.equal(obj.D.line2);
+            done();
+        });
+    });
+
+    it('should not deserialize if condition is not met', done => {
+        const obj = {
+            C: false,
+            D: {
+                line1: 'a',
+                line2: 'b'
+            }
+        };
+        const NO_MAP = <Address>{};
+
+        of(obj).pipe(
+            conditionaMapJsonModel(v => v.C, Address, v => v.D, NO_MAP)
+        ).subscribe(v => {
+            expect(v).to.equal(NO_MAP);
+            done();
+        });
+    });
+
+    it('should deserialize array if condition is met', done => {
+        const obj = {
+            D: <Address>{
+                line1: 'a',
+                line2: 'b'
+            }
+        };
+
+        const obj2 = {
+            D: <Address>{
+                line1: 'c',
+                line2: 'd'
+            }
+        };
+
+        const d = [obj, obj2];
+
+        of(d).pipe(conditionaMapJsonArray(v => v.length === 2, Address, x => x.D)).subscribe(a => {
+            expect(a).to.be.instanceOf(Array);
+
+            a.forEach((v, i) => {
+                expect(v).to.be.instanceOf(Address);
+                expect(v.line1).to.equal(d[i].D.line1);
+                expect(v.line2).to.equal(d[i].D.line2);
+            });
+
+            done();
+        });
+    });
+
+    it('should not deserialize array if condition is not met', done => {
+        const obj = {
+            D: <Address>{
+                line1: 'a',
+                line2: 'b'
+            }
+        };
+
+        const obj2 = {
+            D: <Address>{
+                line1: 'c',
+                line2: 'd'
+            }
+        };
+
+        const d = [obj, obj2];
+
+        of(d).pipe(conditionaMapJsonArray(v => v.length === 3, Address, x => x.D)).subscribe(a => {
+            expect(a).to.be.instanceOf(Array);
+            expect(a.length).to.equal(0);
             done();
         });
     });
