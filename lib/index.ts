3a5b3d250bc9cb2393d00dc85109ce8b99d196d7
Canton Davide <davide.canton5@gmail.com>
2019-01-11 12:02:54 +0100

    handled null values and rename

--- lib/index.ts
+++ lib/index.ts
@@ -2,18 +2,18 @@ import { Constructable, JsonMapper } from 'at-json';
 import { Observable, OperatorFunction } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export function mapModel<T, U>(ctor: Constructable<U>): OperatorFunction<T, U> {
+export function mapJsonModel<T, U>(ctor: Constructable<U>): OperatorFunction<T, U> {
     return (source: Observable<T>) => {
         return source.pipe(
-            map(v => JsonMapper.deserialize(ctor, v))
+            map(v => JsonMapper.deserialize(ctor, v || {}))
         );
     };
 }
 
-export function mapModelArray<T, U>(ctor: Constructable<U>): OperatorFunction<T[], U[]> {
+export function mapJsonArray<T, U>(ctor: Constructable<U>): OperatorFunction<T[] | null, U[]> {
     return (source: Observable<T[]>) => {
         return source.pipe(
-            map(v => JsonMapper.deserializeArray(ctor, v))
+            map(v => JsonMapper.deserializeArray(ctor, v || []))
         );
     };
 }
